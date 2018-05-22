2d74bf7c2d094e80856a23e36be0cb3250c9261f
Denise Pendleton <dpendl1@hotmail.com>
2018-05-22 16:42:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ function calculatesFarePrice(start, end){
 
   if (distance <= 400){
     return 0
-  } else if (distance >= 400 && distance < 2000) {
+  } else if (distance >= 400 && distance <= 2000) {
     fare = (distance - 400) * price
     return fare
-  } else if (distance > 2000){
+  } else if (distance >= 2000 && distance <= 2500){
     fare = 25
     return fare
   } else {
