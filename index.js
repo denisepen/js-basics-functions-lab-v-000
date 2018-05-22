6f04e138fdb09a78c8cf57d7634b19ecba98b6c6
Denise Pendleton <dpendl1@hotmail.com>
2018-05-22 16:09:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file!
-let distInBlocks
+let distInBlocks;
+let fbLocation = 42;
 function distanceFromHqInBlocks(cust_location){
-  
+  Math.abs(distInBlocks = fbLocation - cust_location);
 }
